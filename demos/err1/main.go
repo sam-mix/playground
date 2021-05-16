8eb148410ed2182776cc797844b13111334e5b27
Sam mix <1045262335@qq.com>
2021-05-16 15:57:20 +0800

    x

--- demos/err1/main.go
+++ demos/err1/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"errors"
 	"playground/model"
 	"playground/util"
+
+	"gorm.io/gorm"
 )
 
 func main() {
 	db := util.Conn()
 
-	db.Table("t_bee_true").Where("true = true").First(&model.Bee{})
-
+	// db.Table("t_bee_true").Where("true = true").First(&model.Bee{})
+	// db.Table("t_bee_true").Where("1 = 1").First(&model.Bee{})
+	if err := db.Table("t_bee_true").Where("1 = 1").First(&model.Bee{}).Error; err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		panic(err)
+	}
 }
