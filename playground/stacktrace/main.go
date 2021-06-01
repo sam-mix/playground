daa7e35f1857522e0a48dff6d0fd16258e4ecab7
mihongkun <mihongkun@guanmai.cn>
2021-06-02 01:22:54 +0800

    x

--- playground/stacktrace/main.go
+++ playground/stacktrace/main.go
@@ -1,35 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
-	"github.com/pkg/errors"
+	pkgerrors "github.com/pkg/errors"
 )
 
 func fn4() error {
 	e1 := errors.New("error")
 
+	// e1 := pkgerrors.New("error")
 	return fn3(e1)
 }
 
 func fn1(e error) error {
-	return errors.Wrap(e, "inner")
+	return pkgerrors.Wrap(e, "inner")
 }
 
 func fn2(e error) error {
-	return fn1(errors.Wrap(e, "middle"))
+	return fn1(pkgerrors.Wrap(e, "middle"))
 }
 
 func fn3(e error) error {
-	return fn2(errors.Wrap(e, "outer"))
+	return fn2(pkgerrors.Wrap(e, "outer"))
 }
 
 func main() {
 	type stackTracer interface {
-		StackTrace() errors.StackTrace
+		StackTrace() pkgerrors.StackTrace
 	}
 
-	err, ok := errors.Cause(fn4()).(stackTracer)
+	err, ok := pkgerrors.Cause(fn4()).(stackTracer)
 	if !ok {
 		panic("oops, err does not implement stackTracer")
 	}
